ce7953d2a44bf036164fb03c0f9c7b92561e8d7b
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-11-14 11:50:37 +0700

    Работа с MainActivity

--- app/src/main/java/com/bignerdranch/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.bignerdranch.android.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var okButton: Button
+    private lateinit var priceInput: EditText
+    private lateinit var discountAmountText: TextView
+    private lateinit var discountSeekBar: SeekBar
+    private lateinit var dollarRadioButton: RadioButton
+    private lateinit var euroRadioButton: RadioButton
+    private lateinit var poundRadioButton: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
